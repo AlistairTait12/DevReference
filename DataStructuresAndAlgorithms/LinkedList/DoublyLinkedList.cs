7f323cf5e64a426635bd3d1a718145f2384b877a
Alistair Tait <alistair.tait1@gmail.com>
2023-04-05 22:29:24 +0100

    Added classes related to linked lists

---
+++ DataStructuresAndAlgorithms/LinkedList/DoublyLinkedList.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+
+namespace DataStructuresAndAlgorithms.LinkedList;
+
+public class DoublyLinkedList<T> : ICollection<T>
+{
+    public DoublyLinkedListNode<T> Head { get; private set; }
+
+    public DoublyLinkedListNode<T> Tail { get; private set; }
+
+    public bool GetHead(out T value)
+    {
+    }
+
+    public bool GetTail(out T value)
+    {
+    }
+
+    public int Count => throw new NotImplementedException();
+
+    public bool IsReadOnly => throw new NotImplementedException();
+
+    public void Add(T item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Clear()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Contains(T item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void CopyTo(T[] array, int arrayIndex)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Remove(T item)
+    {
+        throw new NotImplementedException();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ DataStructuresAndAlgorithms/LinkedList/Node.cs
@@ -0,0 +1,15 @@
+﻿namespace DataStructuresAndAlgorithms.LinkedList;
+
+public class Node
+{
+    public Node(int value)
+    {
+        Value = value;
+        Previous = null;
+        Next = null;
+    }
+
+    public Node Previous { get; private set; }
+    public Node Next { get; private set; }
+    public int Value { get; private set; }
+}
